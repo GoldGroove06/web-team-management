85f011d0d0de56cb05160804371354740a9401f4
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2024-08-23 23:30:07 +0530

    Update settings.py

--- tmac/settings.py
+++ tmac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-__*82p^-s7v=394mvhko*ra7+=l_t39h)bq9uxdajtz!-#pjae
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-team-management.onrender.com']
 
 
 # Application definition
