870a3998ed901a9156709e57f3ecb535bb584493
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2024-08-24 00:16:35 +0530

    Update settings.py

--- tmac/settings.py
+++ tmac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-__*82p^-s7v=394mvhko*ra7+=l_t39h)bq9uxdajtz!-#pjae
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0",'web-team-management.onrender.com']
 
 
 # Application definition
@@ -126,3 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "tmacweb.User" 
 
+CSRF_TRUSTED_ORIGINS = ['https://web-team-management.onrender.com']
