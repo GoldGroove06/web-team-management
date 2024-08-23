ba935bdef80e33e55d9ee1ac14e4a528a3052352
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com>
2024-08-24 00:03:19 +0530

    Update settings.py

--- tmac/settings.py
+++ tmac/settings.py
@@ -125,6 +125,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "tmacweb.User" 
-CSRF_TRUSTED_ORIGINS = ["0.0.0.0",'web-team-management.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://web-team-management.onrender.com']
 
 
